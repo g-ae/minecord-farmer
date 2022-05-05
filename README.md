9e80cfea6f10513b4bab71118b272cc501451c57
gonçalo <goncalo-arieira@hotmail.com>
2022-05-05 19:26:56 +0200

    v1.1

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Script written to farm easily with the bot Minecord.
 This bot will do all the human verifications.
 May stop working if the devs change bot verification.
-Use at your own risk !
+Use at your own risk, I am not responsible if you get banned from using their bot !
 
 ### Setup
 * Clone this repo
@@ -15,6 +15,7 @@ git clone https://github.com/gonesteves/minecord-farmer.git
 * Inside config.json file :
   * Write your account's token
   * Write the id the channel in which the bot will be executed
+  * Write the bot's prefix in your guild (default is "m!")
 
 * Execute the script with 
 ```bash
--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
+from sys import prefix
 import time
 import requests
 import json
 
-countfight = 8   # chaque 40 secs
-countchop = 12   # chaque 60 secs
+countfight = 8   # every ~40 seconds
+countchop = 12   # every ~60 seconds
 url = ""
 jsonD = {}
+prefix = ""
 
+# Get last message sent in the channel
 def retrieve_last_message():
     try:
         r = requests.get(f'{url}?limit=1', headers=headers)
@@ -17,13 +20,15 @@ def retrieve_last_message():
         print('The token or channel id is wrong. Stopping...')
         exit()
 
+# Check for anti bot verification
 def verif(content):
     if "Anti bot check" in content:
         verif = content[content.find('`')+1:content.find('`.\n')]
-        send(f'm!verify {eval(verif)}')
+        send(f'{prefix}verify {eval(verif)}')
     elif "Please complete the verification first" in content:
         completeVerif()
 
+# Used if the verification message has already passed so you need to check for recent messages
 def completeVerif():
     r = requests.get(f'{url}?limit=10', headers=headers)
     response = json.loads(r.text)
@@ -32,32 +37,36 @@ def completeVerif():
             verif(value['content'])
             break
 
+# Send a message in the channel provided in config.json
 def send(message):
     requests.post(url, data={'content': message}, headers=headers)
-    print(message)
+    # logs the sent message in the terminal so you can check if you need
+    print(message) # you can't comment this line if you don't want anything in your console. (put a # at the start of the line)
 
+# Will only execute if used directly
 if (__name__ == "__main__"):
     with open('./config.json') as jsonFile:
         jsonD = json.load(jsonFile)
-        url = f'https://discord.com/api/v8/channels/{jsonD["channelId"]}/messages'
+        prefix = jsonD["prefix"]
+        url = f'https://discord.com/api/v8/channels/{jsonD["channelId"]}/messages' # API version can be updated (check Discord's documentation to see if sending messages will still work)
         headers = {
             'Authorization': jsonD["token"]
         }
     while True:
-        send("m!m")
+        send(f"{prefix}m")
         retrieve_last_message()
         if countfight == 8:
             countfight = 0
-            send("m!f")
+            send(f"{prefix}f")
             retrieve_last_message()
         else:
             countfight += 1
         
         if countchop == 12:
             countchop = 0
-            send("m!c")
+            send(f"{prefix}c")
             retrieve_last_message()
         else:
             countchop += 1
 
-        time.sleep(4.4)
+        time.sleep(5) # every 5 seconds for safety (you can reduce it down to 4.4 and it should keep working like normal)
